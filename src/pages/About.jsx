ecad89da69f0a0c9ed9facaf39f58daa14537b2e
Lazizbek <lazizbektojiboyev0611@gmail.com>
2022-05-04 19:01:19 +0500

    it's okay

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { FaInstagram, FaTelegram } from 'react-icons/fa'
+import { FaInstagram, FaTelegram, FaLinkedinIn } from 'react-icons/fa'
+import { FiArrowUpRight } from 'react-icons/fi'
 
 const About = () => {
   return (
     <div className='hero'>
+      <h1 className='hero-text font-semibold text-4xl info'>My Socials</h1>
       <ul className='about__icon'>
         <li className='about__icon--item'>
           <a href="https://instagram.com/mern.me" target={'_blank'} rel='noreferrer'>
@@ -21,6 +23,24 @@ const About = () => {
             <span />
             <span />
             <FaTelegram className='text-4xl' />
+          </a>
+        </li>
+        <li className='about__icon--item'>
+          <a href="https://www.linkedin.com/in/lazizbek-tojiboyev-24185420b" target={'_blank'} rel='noreferrer'>
+            <span />
+            <span />
+            <span />
+            <span />
+            <FaLinkedinIn className='text-4xl' />
+          </a>
+        </li>
+        <li className='about__icon--item'>
+          <a href="https://laziz-dev.netlify.app" target={'_blank'} rel='noreferrer'>
+            <span />
+            <span />
+            <span />
+            <span />
+            <FiArrowUpRight className='text-4xl' />
           </a>
         </li>
       </ul>
